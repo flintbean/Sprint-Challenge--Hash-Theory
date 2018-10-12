6bb38bea55efb80569c27fd64b33c2873cc72e0c
Austin Merando <ajmerando1@yahoo.com>
2018-10-12 12:46:01 -0700

    finished!

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,10 +1,23 @@
 def reconstruct_trip(tickets):
     d = {}
+    flight = []
 
     for ticket in tickets:
         d[ticket[0]] = ticket[1]
-    
-    
+    flight.append(d[None])
+
+    current = d[None]
+
+    while current is not None:
+      if current in d:
+        current = d[current]
+        if current is not None:
+          flight.append(current)
+      else:
+        return []
+    return flight
+
+
 
 if __name__ == '__main__':
     # You can write code here to test your implementation using the Python repl
